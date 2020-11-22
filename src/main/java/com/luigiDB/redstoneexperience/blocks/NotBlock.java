3eaf030819a1a1cc4e5431860f5fcd84144322ce
luigidb <luigi.debianchi@gmail.com>
2020-11-22 21:19:36 +0100

    Define gate rotation and shape

--- src/main/java/com/luigiDB/redstoneexperience/blocks/NotBlock.java
+++ src/main/java/com/luigiDB/redstoneexperience/blocks/NotBlock.java
@@ -12,10 +12,12 @@ import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
+import java.util.Optional;
+
 public class NotBlock extends Block {
-    protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
     public static final DirectionProperty PROPERTY_FACING = BlockStateProperties.FACING;
 
     public NotBlock(Properties properties) {
@@ -25,22 +27,49 @@ public class NotBlock extends Block {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return SHAPE;
+        switch (state.get(PROPERTY_FACING)) {
+            case DOWN:
+                return Block.makeCuboidShape(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+            case UP:
+                return Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+            case NORTH:
+                return Block.makeCuboidShape(0.0D, 0.0D, 14.0D, 16.0D, 16.0D, 16.0D);
+            case EAST:
+                return Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 2.0D, 16.0D, 16.0D);
+            case SOUTH:
+                return Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 2.0D);
+            case WEST:
+                return Block.makeCuboidShape(14.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+        }
+        return Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
     }
 
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return state.with(PROPERTY_FACING, rot.rotate(state.get(PROPERTY_FACING)));
-    }
-
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.toRotation(state.get(PROPERTY_FACING)));
-    }
+//    public BlockState rotate(BlockState state, Rotation rot) {
+//        return state.with(PROPERTY_FACING, rot.rotate(state.get(PROPERTY_FACING)));
+//    }
+//
+//    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+//        return state.rotate(mirrorIn.toRotation(state.get(PROPERTY_FACING)));
+//    }
 
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
         builder.add(PROPERTY_FACING);
     }
 
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return this.getDefaultState().with(PROPERTY_FACING, context.getNearestLookingDirection());
+        return this.getDefaultState().with(PROPERTY_FACING, context.getFace());
+    }
+
+    private VoxelShape rotateShape(Direction from, Direction to, VoxelShape shape) {
+        VoxelShape[] buffer = new VoxelShape[]{ shape, VoxelShapes.empty() };
+
+        int times = (to.getHorizontalIndex() - from.getHorizontalIndex() + 4) % 4;
+        for (int i = 0; i < times; i++) {
+            buffer[0].forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = VoxelShapes.or(buffer[1], VoxelShapes.create(1-maxZ, minY, minX, 1-minZ, maxY, maxX)));
+            buffer[0] = buffer[1];
+            buffer[1] = VoxelShapes.empty();
+        }
+
+        return buffer[0];
     }
 }
